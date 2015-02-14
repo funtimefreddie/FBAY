c8cf4ebeb2d5d898f74b7eed5a8034068befd25d
Freddie Waite <f.m.waite@gmail.com>
2015-02-14 10:01:44 +0800

    put AWS in application.yml file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
 
     config.fog_credentials = {
         :provider => 'AWS',
-        :aws_access_key_id => Rails.application.secrets.aws_access_key_id,
-        :aws_secret_access_key => Rails.application.secrets.aws_secret_access_key,
+        :aws_access_key_id => ENV['aws_access_key_id'],
+        :aws_secret_access_key => ENV['aws_secret_access_key'],
         :region => 'eu-west-1' # change
     }
     config.fog_directory = 'freddiesmedia' # change
