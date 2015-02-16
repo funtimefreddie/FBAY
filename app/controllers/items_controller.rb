e96e78417fa3a1158d0826a3cbb50e427432736c
Freddie Waite <f.m.waite@gmail.com>
2015-02-16 11:31:25 +0800

    added new pages to header

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
-  layout 'header'
+  # layout 'header'
 
   # GET /items
   # GET /items.json
