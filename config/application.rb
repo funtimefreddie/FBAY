250d677ad0e85f45d16dec116d22b6bfbc2fa8d4
Freddie Waite <f.m.waite@gmail.com>
2015-02-24 14:12:07 +0800

    I HATE COMPILE

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,6 @@ module Fbay
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.initialize_on_precompile = false
+    
   end
 end
