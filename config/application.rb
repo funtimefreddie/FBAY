ec122f2116effb5970258f662f4ce51957fb36b4
Freddie Waite <f.m.waite@gmail.com>
2015-02-24 14:05:07 +0800

    still pissing around with compile

--- config/application.rb
+++ config/application.rb
@@ -3,13 +3,14 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Fbay
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -25,5 +26,6 @@ module Fbay
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
   end
 end
