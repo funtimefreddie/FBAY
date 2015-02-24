6e818001e25b4cde6c496d28c76c3d06edee7123
Freddie Waite <f.m.waite@gmail.com>
2015-02-24 13:31:16 +0800

    moved config in enviroment.rb

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-config.assets.initialize_on_precompile = false
 
 
 # Require the gems listed in Gemfile, including any gems
@@ -10,6 +9,8 @@ Bundler.require(*Rails.groups)
 
 module Fbay
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
